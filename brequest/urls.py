65cf6763f7f65c3f06d64a7428e4d62fb24c6921
Fly744055970 <Fly@163.com>
2019-03-09 10:09:34 +0800

    1. 请求request  参数  /2008/beijing/
    1.1 正则匹配 urls.py
    1.2 views.py  index(request,year,city)
    1.3 ?P<regName>

--- brequest/urls.py
+++ brequest/urls.py
@@ -7,5 +7,12 @@ from . import views
 urlpatterns = [
 
     # 末尾加不加  /  看公司要求
-    url(r'^brequest/$', views.login, name="login")
+    url(r'^brequest/$', views.login, name="login"),
+
+    #     1.  2008/beijing/拼接路径参数 直接正则匹配
+    # url(r'^brequest/(\d{4})/([a-z]+)/$', views.login_connect),
+
+
+    #     2.  2008/beijing/拼接路径参数 直接正则匹配  给正则起名字 获取的时候就可以不根据顺序
+    url(r'^brequest/(?P<year>\d{4})/(?P<city>[a-z]+)/$', views.login_connect),
 ]
--- brequest/views.py
+++ brequest/views.py
@@ -1,7 +1,16 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
+# 1.解析 拼接路径的参数   /1999/beijing/
+
+def login_connect(request, city, year):
+    print('解析拼接路径的参数----年份:', year)
+    print('解析拼接路径的参数----城市:', city)
+
+    return HttpResponse('1.1 10/20/拼接路径参数')
+
+
 # Create your views here.
 def login(request):
-
     return HttpResponse('1.研究request请求对象 传参  1.1 10/20/拼接路径 1.2 ?a=10&b=20&a=30 1.3 json xml str 1.4 form 1.5 请求头')
